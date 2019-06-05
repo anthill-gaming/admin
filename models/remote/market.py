58458741fe2ddd7172db92e14a73d35920227dba
Vladimir <x55aah@gmail.com>
2019-06-05 20:28:44 +0300

    updates

--- models/remote/market.py
+++ models/remote/market.py
@@ -1,4 +1,10 @@
 from anthill.platform.remote_models import remote_model_factory
 
 
-__all__ = []
+__all__ = ['Market', 'MarketSeller', 'MarketItem', 'MarketOrder']
+
+
+Market = remote_model_factory('market.Market')
+MarketSeller = remote_model_factory('market.Seller')
+MarketItem = remote_model_factory('market.Item')
+MarketOrder = remote_model_factory('market.Order')
